2c01c7a56e78ccbe2308db919863bce059fcea2f
Vaibhav Agarwal <vaibhav@parcelhero.com>
2025-05-09 08:19:44 +0530

    add static files

--- QRcodeGenerator/Program.cs
+++ QRcodeGenerator/Program.cs
@@ -13,23 +13,43 @@ namespace QRcodeGenerator
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddSingleton(x => new JsonReaderAndWriter());
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    "CorsPolicy",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                );
+            });
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            //// Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //}
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
+            AddDefaultOptions(app);
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static void AddDefaultOptions(WebApplication app)
+        {
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //Clear any DefaultFileNames if already there
+            defaultFilesOptions.DefaultFileNames.Clear();
+            //Add the default HTML Page to the DefaultFilesOptions Instance
+            defaultFilesOptions.DefaultFileNames.Add("index.html");
+            //Setting the Default Files
+            //Pass the DefaultFilesOptions Instance to the UseDefaultFiles Middleware Component
+            app.UseDefaultFiles(defaultFilesOptions);
         }
     }
 }
